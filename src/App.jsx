7a77816e801648ec410e3913a98493ba8486572c
dev0869 <deveshbisht36@gmail.com>
2024-03-16 18:56:02 +0530

    fix:fit in view

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import ReactFlow from 'reactflow';
-import 'reactflow/dist/style.css';
-import { initialEdges, initialNodes } from './nodes';
+import { useState } from "react";
+import ReactFlow from "reactflow";
+import "reactflow/dist/style.css";
+import { initialEdges, initialNodes } from "./nodes";
 const App = () => {
-  const [poke, setPoke] = useState(false)
+  const [poke, setPoke] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   const handleclick = (event, node) => {
@@ -13,13 +13,22 @@ const App = () => {
   };
 
   return (
-    <div style={{ width: '100%', height: '100vh', border: '2px solid gray' }}>
-      <ReactFlow nodes={initialNodes} onNodeMouseEnter={(event, node) => handleclick(event, node)}
-        edges={initialEdges} />
-      {poke && <div style={{ position: 'absolute', top: position.y, left: position.x }}>{'hiiiiiii'}</div>}
-
+    <div style={{ width: "100%", height: "100vh", border: "2px solid gray" }}>
+      <ReactFlow
+        nodes={initialNodes}
+        fitView
+        onNodeMouseEnter={(event, node) => handleclick(event, node)}
+        edges={initialEdges}
+      />
+      {poke && (
+        <div
+          style={{ position: "absolute", top: position.y, left: position.x }}
+        >
+          {"hiiiiiii"}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
