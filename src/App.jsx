b14173c72dda7ae4b7ea271f077afabf504210c8
dev0869 <deveshbisht36@gmail.com>
2024-03-18 14:20:55 +0530

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,19 @@
-// import { useState } from "react";
-// import ReactFlow from "reactflow";
-// import "reactflow/dist/style.css";
-// import { initialEdges, initialNodes } from "./nodes";
-// const App = () => {
-//   const [poke, setPoke] = useState(false);
-//   const [position, setPosition] = useState({ x: 0, y: 0 });
-
-//   const handleclick = (event, node) => {
-//     setPoke(true);
-//     setPosition({ x: event.clientX, y: event.clientY });
-//     console.log(node);
-//   };
-
-//   return (
-//     <div style={{ width: "100%", height: "100vh", border: "2px solid gray" }}>
-//       <ReactFlow
-//         nodes={initialNodes}
-//         fitView
-//         onNodeMouseEnter={(event, node) => handleclick(event, node)}
-//         onNodeMouseLeave={() => setPoke(false)}
-
-//         edges={initialEdges}
-//       />
-//       {poke && (
-//         <div
-//           style={{ position: "absolute", backgroundColor: "white", top: position.y, left: position.x, width: '100px', height: '100px' }}
-//         >
-//           {"hiiiiiii"}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
 import { useState } from "react";
 import ReactFlow from "reactflow";
 import "reactflow/dist/style.css";
 import { initialEdges, initialNodes } from "./nodes";
 
 const App = () => {
-  const [poke, setPoke] = useState(false);
-
+  const [enablePos, setEnablePos] = useState(false);
+  const [lable, setLable] = useState('')
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  console.log(position);
-  const handleclick = (event) => {
-
+  
+  const handleclick = (event, node) => {
+    setLable(node.data.label);
     setPosition({ x: event.clientX, y: event.clientY });
-    setPoke(true);
+    setEnablePos(true);
   };
 
-  const handleMouseLeave = () => {
-    setPoke(false);
-  };
 
   return (
     <div style={{ width: "100%", height: "100vh", border: "2px solid gray" }}>
@@ -62,17 +21,30 @@ const App = () => {
         nodes={initialNodes}
         fitView
         onNodeMouseEnter={(event, node) => handleclick(event, node)}
-        onNodeMouseLeave={handleMouseLeave}
+        onNodeMouseLeave={() => setEnablePos(false)}
         edges={initialEdges}
       />
 
-      <div onMouseEnter={() => setPoke(true)}
-        style={{ position: "absolute", opacity: poke ? 1 : 0, transition: '0.5s', backgroundColor: "white", top: poke ? position.y : '', left: poke ? position.x : '', width: '100px', height: '100px' }}
+      <div
+        onMouseEnter={() => setEnablePos(true)}
+        style={{
+          position: "absolute",
+          opacity: enablePos ? 1 : 0,
+          transition: "0.5s",
+          backgroundColor: "gray",
+          top: enablePos ? position.y : "",
+          left: enablePos ? position.x : "",
+          width: "300px",
+          height: "100px",
+        }}
       >
-        {"hiiiiiii"}
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <p>Name:{lable}</p>
+          <p>Total Review</p>
+          <p>Positive Review</p>
+          <p>Comments</p>
+        </div>
       </div>
-
-
     </div>
   );
 };
